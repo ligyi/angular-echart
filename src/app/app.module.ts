9152adc18199bf6ed034982acb36e896b318f9bd
ligyi <ligyig@gmail.com>
2019-09-13 08:50:15 -0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BasicComponent } from './basic-chart/bar-chart';
+import { NgxEchartsModule } from 'ngx-echarts';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxEchartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
